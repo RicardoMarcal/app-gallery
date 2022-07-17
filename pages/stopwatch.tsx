6fb1e5f01fcc0b7100684e7fea18526500fbe549
unknown <ricardomarcal02@hotmail.com>
2022-07-17 11:13:16 -0300

    stopwatch update

--- pages/stopwatch.tsx
+++ pages/stopwatch.tsx
@@ -5,9 +5,17 @@ import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
+interface mark {
+  time: Date,
+  hours: Number,
+  minutes: Number,
+  seconds: Number,
+  milisseconds: Number
+}
+
 const Stopwatch: NextPage = () => {
   const [start, setStart] = useState(Date.now())
-  const [distance, setDistance] = useState(0)
+  const [marks, setMarks] = useState([] as mark[])
   const [hours, setHours] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
@@ -28,8 +36,24 @@ const Stopwatch: NextPage = () => {
   }, [start])
   
 
-  const handleStart = () => {
+  const handleReset = () => {
     setStart(Date.now())
+    let distance = Date.now() - start
+    setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+    setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)))
+    setSeconds(Math.floor((distance % (1000 * 60)) / 1000))
+    setMilisseconds(Math.floor(distance % 1000))
+    setMarks([])
+  }
+
+  const handleSave = () => {
+    setMarks([...marks, {
+      time: new Date(Date.now()),
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+      milisseconds: milisseconds
+    }])
   }
 
   return (
@@ -50,12 +74,33 @@ const Stopwatch: NextPage = () => {
             {' '+('00' + seconds).slice(-2)}s
             {' '+('000' + milisseconds).slice(-3)}ms
           </p>
-          <input
-            type="button"
-            value="Reset"
-            onClick={handleStart}
-            className="p-1 mt-5 w-32 text-center bg-slate-100 rounded-md cursor-pointer"
-          />
+          <section className='flex gap-3'>
+            <input
+              type="button"
+              value="Reset"
+              onClick={handleReset}
+              className="p-1 mt-5 w-32 text-center bg-slate-100 rounded-md cursor-pointer"
+            />
+            <input
+              type="button"
+              value="Save"
+              onClick={handleSave}
+              className="p-1 mt-5 w-32 text-center bg-slate-100 rounded-md cursor-pointer"
+            />
+          </section>
+          <section className='flex flex-col-reverse justify-start mt-4'>
+            {marks.map(({time, hours, minutes, seconds, milisseconds}, key) => (
+              <div key={time.getTime()} className='flex gap-1'>
+                {key+1 + ') '+ time.toLocaleDateString() + ' | ' + time.toLocaleTimeString() + ' - '}
+                <div className='font-bold'>
+                  {hours > 0 ? ('00' + hours).slice(-2) + 'h' : ''}
+                  {minutes > 0 ? ' '+('00' + minutes).slice(-2) + 'm' : ''}
+                  {seconds > 0 ? ' '+('00' + seconds).slice(-2) + 's' : ''}
+                  {milisseconds > 0 ? ' '+('000' + milisseconds).slice(-3) + 'ms' : ''}
+                </div>
+              </div>
+            ))}
+          </section>
         </div>
 
       </main>
